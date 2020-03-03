214759da46fdcee597c538b62a033550312418de
David Třetina <tretinadavid@gmail.com>
2020-03-03 21:50:52 +0100

    Added basic shooting
    Co-Authored-By: Tobiáš Karpeta <tobiaskarpeta@users.noreply.github.com>

---
+++ Spacie Rocket/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 1;
+
+    public void TakeDamage(int damage){
+        health -= damage;
+        if(health<=0){
+            Die();
+        }
+    }
+
+    public void Die(){
+        Destroy(gameObject);
+    }
+}
---
+++ Spacie Rocket/Assets/Shooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject player;
+    public int damage;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+             Shoot();
+         }
+    }
+
+    void Shoot(){
+        Instantiate(bullet,transform.position,transform.rotation);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo){
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy!=null){
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
