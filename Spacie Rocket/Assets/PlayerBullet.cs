fe7022adbdadc6cc17fea8ff79fa2e1d456342df
Tobiáš Karpeta <tobiaskarpeta@hotmail.com>
2020-03-06 21:35:19 +0100

    ???

---
+++ Spacie Rocket/Assets/PlayerBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+
+    public float speed = 2f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+}
